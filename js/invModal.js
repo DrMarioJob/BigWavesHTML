2f4cd219f1dd95d25600dbbe15ac871614876836
disco1988 <prowebmasti@gmail.com>
2024-11-23 01:14:19 +0200

    edits site

--- js/invModal.js
+++ js/invModal.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Открытие модального окна по клику на invInfo
     document.body.addEventListener("click", (event) => {
-        if (event.target.closest(".invInfo")) {
-            const button = event.target.closest(".invInfo");
-            const infoId = button.getAttribute("data-info");
+        const invInfoBlock = event.target.closest(".invAllRow");
+        if (invInfoBlock) {
+            const infoId = invInfoBlock.getAttribute("data-info");
             const modal = document.getElementById(`invModal${infoId}`);
             if (modal) {
                 document.querySelectorAll(".invModal").forEach(modal => modal.classList.remove("active"));
@@ -11,32 +12,35 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // Закрытие модального окна по клику на closeModalInv
     document.body.addEventListener("click", (event) => {
-        if (event.target.closest(".closeModalInv")) {
-            const button = event.target.closest(".closeModalInv");
-            const modal = button.closest(".invModal");
+        const closeButton = event.target.closest(".closeModalInv");
+        if (closeButton) {
+            const modal = closeButton.closest(".invModal");
             if (modal) {
                 modal.classList.remove("active");
             }
         }
     });
 
+    // Закрытие активного модального окна по клику вне него и вне invInfo
     document.body.addEventListener("click", (event) => {
         const activeModal = document.querySelector(".invModal.active");
-        if (activeModal && !event.target.closest(".invModal") && !event.target.closest(".invInfo")) {
+        if (activeModal && !event.target.closest(".invModal") && !event.target.closest(".invAllRow")) {
             activeModal.classList.remove("active");
         }
     });
 
+    // Убираем все активные модальные окна при загрузке страницы
     document.querySelectorAll(".invModal").forEach(modal => modal.classList.remove("active"));
 
+    // Убираем все активные модальные окна при изменении состояния owl-carousel
     $(".owl-carousel").on('initialized.owl.carousel changed.owl.carousel', function () {
         document.querySelectorAll(".invModal").forEach(modal => modal.classList.remove("active"));
     });
 
+    // Инициализация owl-carousel
     $(document).ready(function () {
-        $(".owl-carousel").owlCarousel({
-
-        });
+        $(".owl-carousel").owlCarousel({});
     });
 });
